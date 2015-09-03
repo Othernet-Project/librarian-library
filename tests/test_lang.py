2c297eba8f09b69941d7746fa02ae8b98f28002f
Andrean Franc <andrean.franc@gmail.com>
2015-09-03 14:44:49 +0200

    Add missing test

---
+++ tests/test_lang.py
@@ -0,0 +1,7 @@
+from librarian_library import lang as mod
+
+
+def test_rtl_property(*ignored):
+    """ RTL property returns True for RTL languages """
+    assert not mod.is_rtl('en')
+    assert mod.is_rtl('ar')
