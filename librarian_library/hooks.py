72292c5d2a904a4b93603ced2435834436ea9348
Andrean Franc <andrean.franc@gmail.com>
2015-10-28 15:21:26 +0100

    Disable timezone setup step

--- librarian_library/hooks.py
+++ librarian_library/hooks.py
@@ -18,18 +18,6 @@ def initialize(supervisor):
                           method='POST',
                           index=1,
                           test=setup.is_language_invalid)
-    setup_wizard.register('datetime',
-                          setup.setup_datetime_form,
-                          template='setup/step_datetime.tpl',
-                          method='GET',
-                          index=2,
-                          test=setup.has_bad_tz)
-    setup_wizard.register('datetime',
-                          setup.setup_datetime,
-                          template='setup/step_datetime.tpl',
-                          method='POST',
-                          index=2,
-                          test=setup.has_bad_tz)
     setup_wizard.register('import_content',
                           setup.setup_import_content_form,
                           template='setup/step_import_content.tpl',
