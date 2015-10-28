9b7335dce00bce244edf9f87f9813434cba11958
Andrean Franc <andrean.franc@gmail.com>
2015-10-28 15:19:47 +0100

    Set current language immediately when choosing default locale

--- librarian_library/setup.py
+++ librarian_library/setup.py
@@ -6,6 +6,7 @@ import pytz
 from bottle import request
 
 from librarian_core.contrib.i18n.utils import (set_default_locale,
+                                               set_current_locale,
                                                get_enabled_locales)
 
 from .forms import get_language_form, SetupDateTimeForm, SetupImportContentForm
@@ -33,6 +34,7 @@ def setup_language():
     lang = form.processed_data['language']
     request.app.supervisor.exts.setup.append({'language': lang})
     set_default_locale(lang)
+    set_current_locale(lang)
     return dict(successful=True, language=lang)
 
 
