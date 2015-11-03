9c40eebaf675b460188ef3bbf0d1721256449066
Ben Ennis <ben@outernet.is>
2015-11-03 14:40:41 -0800

    adjusted form so language from URL takes precedence over default - see #8

--- librarian_library/forms.py
+++ librarian_library/forms.py
@@ -1,20 +1,29 @@
 import pytz
 
+from bottle import request
 from bottle_utils import form
 from bottle_utils.i18n import lazy_gettext as _
 
 from librarian_core.contrib.i18n.consts import LANGS
 
 
+def get_default_locale(locales, config):
+    def default_locale():
+        if request.locale in locales:
+            return request.locale
+        else:
+            return config.get('i18n.default_locale', 'en')
+    return default_locale
+
+
 def get_language_form(config):
-    default_locale = config.get('i18n.default_locale', 'en')
     locales = config.get('i18n.locales', ['en'])
     ui_languages = [(code, name) for code, name in LANGS if code in locales]
 
     class SetupLanguageForm(form.Form):
         # Translators, used as label for language
         language = form.SelectField(_('Language'),
-                                    value=default_locale,
+                                    value=get_default_locale(locales, config),
                                     validators=[form.Required()],
                                     choices=ui_languages)
 
