81c1653ac11d2d8b9f53fbd4b7579ba89f514b8b
Andrean Franc <andrean.franc@gmail.com>
2015-11-25 14:48:25 +0100

    Obliterating tag related code

--- librarian_library/routes/content.py
+++ librarian_library/routes/content.py
@@ -8,10 +8,10 @@ This software is free software licensed under the terms of GPLv3. See COPYING
 file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 """
 
-from bottle import request, redirect, abort
+from bottle import request, redirect
 from bottle_utils.ajax import roca_view
 from bottle_utils.html import set_qparam, urlunquote
-from bottle_utils.i18n import lazy_gettext as _, i18n_url
+from bottle_utils.i18n import i18n_url
 
 from librarian_content.decorators import with_meta
 from librarian_content.library import metadata
@@ -23,17 +23,16 @@ from ..helpers import open_archive
 
 
 @cached(prefix='content', timeout=300)
-def content_count(query, lang, tag, content_type):
+def content_count(query, lang, content_type):
     archive = open_archive()
-    return archive.get_count(query, lang, tag, content_type)
+    return archive.get_count(query, lang, content_type)
 
 
 @cached(prefix='content', timeout=300)
-def filter_content(query, lang, tag, content_type, offset, limit):
+def filter_content(query, lang, content_type, offset, limit):
     archive = open_archive()
     raw_metas = archive.get_content(terms=query,
                                     lang=lang,
-                                    tag=tag,
                                     content_type=content_type,
                                     offset=offset,
                                     limit=limit)
@@ -54,28 +53,15 @@ def content_list():
     content_type = request.params.get('content_type')
     if content_type not in metadata.CONTENT_TYPES:
         content_type = None
-    # parse tag filter
-    archive = open_archive()
-    try:
-        tag = int(request.params.get('tag'))
-    except (TypeError, ValueError):
-        tag = None
-        tag_name = None
-    else:
-        try:
-            tag_name = archive.get_tag_name(tag)['name']
-        except (IndexError, KeyError):
-            abort(404, _('Specified tag was not found'))
     # parse pagination params
     page = Paginator.parse_page(request.params)
     per_page = Paginator.parse_per_page(request.params)
     # get content list filtered by above parsed filter params
-    item_count = content_count(query, lang, tag, content_type)
+    item_count = content_count(query, lang, content_type)
     pager = Paginator(item_count, page, per_page)
     offset, limit = pager.items
     metas = filter_content(query,
                            lang,
-                           tag,
                            content_type,
                            offset=offset,
                            limit=limit)
@@ -86,9 +72,6 @@ def content_list():
                 chosen_lang=lang,
                 content_types=metadata.CONTENT_TYPES,
                 chosen_content_type=content_type,
-                tag=tag_name,
-                tag_id=tag,
-                tag_cloud=archive.get_tag_cloud(),
                 base_path=i18n_url('content:list'),
                 view=request.params.get('view'))
 
